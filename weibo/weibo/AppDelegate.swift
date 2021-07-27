937f64c25c45d9a9846472159969ebc470d0a102
MeikiZero <wangfa102501>
2021-07-27 10:25:26 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  weibo
+//
+//  Created by wangfa on 2021/7/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
